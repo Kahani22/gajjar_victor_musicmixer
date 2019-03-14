550ada90b86fe93f6e20b2c7dacbca978fd80f20
rayn-victor <43250423+rayn-victor@users.noreply.github.com>
2019-03-13 22:26:37 -0400

    fix bugs

--- js/main.js
+++ js/main.js
@@ -1,28 +1,7 @@
 (() => {
 	console.log("Javascript is linked up");
 
-	/*let strings = document.querySelectorAll('.eadgbe');
-
-	function guitarStrumming() {
-		strings.querySelectorAll('.eadgbe').forEach(div => {
-			div.addEventListener("dragstart", function(e) {
-				console.log('draggin...')
-			e.dataTransfer.setData("text/plain", this.id);
-			});
-		});
-	}*/
-
-	/*var audio = document.getElementById(".eadgbe");*/
-
-	/*var strings = [0, 1, 2, 3, 4, 5]
-
-	var strings = document.querySelectorAll(".eadgbe");
-
-	strings.addEventListener("click", playAudio)
-		console.log("WUBDFAOUBFOANF");
-		function playAudio() {
-	  audio.play();
-	};*/
+	/*PLAYING AUDIO INDIVIDUALLY*/
 	let aud0 = document.querySelector('#audio0'),
 		aud1 = document.querySelector('#audio1'),
 		aud2 = document.querySelector('#audio2'),
@@ -37,29 +16,35 @@
 		play5 = document.querySelector('#string6');
 
 	function playAudio0() {
+		aud0.currentTime = 0;
 		aud0.play();
 	}
 	function playAudio1() {
+		aud1.currentTime = 0;
 		aud1.play();
 	}
 	function playAudio2() {
+		aud2.currentTime = 0;
 		aud2.play();
 	}
 	function playAudio3() {
+		aud3.currentTime = 0;
 		aud3.play();
 	}
 	function playAudio4() {
+		aud4.currentTime = 0;
 		aud4.play();
 	}
 	function playAudio5() {
+		aud5.currentTime = 0;
 		aud5.play();
 	}
 
-	play0.addEventListener("click", playAudio0);
-	play1.addEventListener("click", playAudio1);
-	play2.addEventListener("click", playAudio2);
-	play3.addEventListener("click", playAudio3);
-	play4.addEventListener("click", playAudio4);
-	play5.addEventListener("click", playAudio5);
+	play0.addEventListener("mouseover", playAudio0);
+	play1.addEventListener("mouseover", playAudio1);
+	play2.addEventListener("mouseover", playAudio2);
+	play3.addEventListener("mouseover", playAudio3);
+	play4.addEventListener("mouseover", playAudio4);
+	play5.addEventListener("mouseover", playAudio5);
 
 })();
