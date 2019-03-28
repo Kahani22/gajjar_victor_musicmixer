be15c43aedf5524e837b1c36b3633a9a48562b34
rayn-victor <43250423+rayn-victor@users.noreply.github.com>
2019-03-27 20:48:35 -0400

    add files, update new design

--- js/main.js
+++ js/main.js
@@ -1,44 +1,120 @@
 (() => {
-	console.log("Javascript is linked up");
+	console.log("Ready to jam!!!");
 
-/* guitar strings play chords*/
-	let aud0 = document.querySelector('#audio0'),
-		aud1 = document.querySelector('#audio1'),
-		aud2 = document.querySelector('#audio2'),
-		aud3 = document.querySelector('#audio3'),
-		aud4 = document.querySelector('#audio4'),
-		aud5 = document.querySelector('#audio5'),
-		play0 = document.querySelector('#string1');
-		play1 = document.querySelector('#string2');
-		play2 = document.querySelector('#string3');
-		play3 = document.querySelector('#string4');
-		play4 = document.querySelector('#string5');
-		play5 = document.querySelector('#string6');
+	let		drumsticks  = document.querySelector(".drumsticks");
+        dropzones   = document.querySelector(".dropzones");
+      	audio	     	= document.querySelector(".audio");
+      	audio2      = document.querySelector(".audio2");
+        audio3      = document.querySelector(".audio3");
+        audio4      = document.querySelector(".audio4");
+        audio5      = document.querySelector(".audio5");
+        audio6      = document.querySelector(".audio6");
+        audio7      = document.querySelector(".audio7");
+        audio8      = document.querySelector(".audio8");
+        audio9      = document.querySelector(".audio9");
+        audio10      = document.querySelector(".audio10");
 
-	function playAudio0() {
-		aud0.play();
-	}
-	function playAudio1() {
-		aud1.play();
-	}
-	function playAudio2() {
-		aud2.play();
-	}
-	function playAudio3() {
-		aud3.play();
-	}
-	function playAudio4() {
-		aud4.play();
-	}
-	function playAudio5() {
-		aud5.play();
-	}
+  let crashDropzone 	   = document.querySelector('#crash');
+  	  hiTomDropzone 	   = document.querySelector('#hi-tom');
+      snareDropzone      = document.querySelector('#snare');
+      medTomDropzone     = document.querySelector('#med-tom');
+      floorTomDropzone   = document.querySelector('#floor-tom');
+      bassDrumDropzone   = document.querySelector('#bass-drum');
+      crash2Dropzone     = document.querySelector('#crash2');
+      rideDropzone     = document.querySelector('#ride');
+      openHatDropzone     = document.querySelector('#open-hat');
+      kickDropzone     = document.querySelector('#kick');
 
-	play0.addEventListener("click", playAudio0); /*hover not working*/
-	play1.addEventListener("click", playAudio1);
-	play2.addEventListener("click", playAudio2);
-	play3.addEventListener("click", playAudio3);
-	play4.addEventListener("click", playAudio4);
-	play5.addEventListener("click", playAudio5);
+  /*function createDrumstick(drumstick) {
+    let newDrumstick = `<img draggable id="drumstick1" class="drumstick" src="images/drumstick1.svg" alt="drumstick">`;
+
+        drumsticks.innerHTML += newDrumstick;
+
+    initDrag();
+  } */
+
+  function initDrag() {
+    drumsticks.querySelector('img');
+    img.addEventListener("dragstart", function(e) {
+      console.log('about to make some noise!')
+    });
+  }
+
+  dropzones.addEventListener("dragover", function(e) {
+      e.preventDefault();
+      console.log("that's the beat!");
+  });
+
+  crashDropzone.addEventListener("drop", function(e) {
+      e.preventDefault();
+      console.log("nice!");
+      audio.currentTime = 0;
+      audio.play();
+  });
+
+  hiTomDropzone.addEventListener("drop", function(e) {
+      e.preventDefault();
+      console.log("nice!");
+      audio2.currentTime = 0;
+      audio2.play();
+  });
+
+  snareDropzone.addEventListener("drop", function(e) {
+      e.preventDefault();
+      console.log("nice!");
+      audio3.currentTime = 0;
+      audio3.play();
+  });
+
+  medTomDropzone.addEventListener("drop", function(e) {
+      e.preventDefault();
+      console.log("nice!");
+      audio4.currentTime = 0;
+      audio4.play();
+  });
+
+  floorTomDropzone.addEventListener("drop", function(e) {
+      e.preventDefault();
+      console.log("nice!");
+      audio5.currentTime = 0;
+      audio5.play();
+  });
+
+  bassDrumDropzone.addEventListener("drop", function(e) {
+      e.preventDefault();
+      console.log("nice!");
+      audio6.currentTime = 0;
+      audio6.play();
+  });
+
+  crash2Dropzone.addEventListener("drop", function(e) {
+      e.preventDefault();
+      console.log("nice!");
+      audio7.currentTime = 0;
+      audio7.play();
+  });
+
+  rideDropzone.addEventListener("drop", function(e) {
+      e.preventDefault();
+      console.log("nice!");
+      audio8.currentTime = 0;
+      audio8.play();
+  });
+
+  openHatDropzone.addEventListener("drop", function(e) {
+      e.preventDefault();
+      console.log("nice!");
+      audio9.currentTime = 0;
+      audio9.play();
+  });
+
+  kickDropzone.addEventListener("drop", function(e) {
+      e.preventDefault();
+      console.log("nice!");
+      audio10.currentTime = 0;
+      audio10.play();
+  });
+
+  /*createDrumstick(0);*/
 
 })();
